06f95624c5fe6bbb0e5c4cf04506c2bb6f71c6b0
Mimbrera-12 <88411420+ManuelMimbrera@users.noreply.github.com>
2021-11-04 02:03:25 -0600

    Commit Message

--- app/src/main/java/com/example/practica1/ui/productos/EliminarProductos.kt
+++ app/src/main/java/com/example/practica1/ui/productos/EliminarProductos.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.findNavController
 import com.example.practica1.R
 import com.example.practica1.base.dbHelper
@@ -29,7 +30,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [EliminarProductos.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EliminarProductos : Fragment() {
+class EliminarProductos : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -43,7 +44,8 @@ class EliminarProductos : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
